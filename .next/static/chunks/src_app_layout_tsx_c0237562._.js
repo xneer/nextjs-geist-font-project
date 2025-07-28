ecdbafe5cea14558cebce3ffb6a537315b42a92f
xneer <neerajsinha14@gmail.com>
2025-07-28 18:38:16 +0530

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1753708095441

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_fb5c6de1._.js",
+  "static/chunks/src_e455f0ba._.js"
+],
+    source: "dynamic"
+});
